25bd24fcb79f26ef70b11a6a67aa25d0dce3cf34
JTEKO <div91@hotmail.fr>
2017-12-10 08:55:38 +0100

    change Login.php

--- Login.php
+++ Login.php
@@ -4,6 +4,10 @@
 <head>
   <meta charset="utf-8" />
   <link rel="stylesheet" href="style/login.css" />
+  <script src="https://code.jquery.com/jquery-3.2.1.js"
+    integrity="sha256-DZAnKJ/6XZ9si04Hgrsxu/8s717jcIzLy3oi35EouyE="
+    crossorigin="anonymous"></script>
+  <script src="script/login.js"></script>
   <title>
     Login
   </title>
@@ -11,9 +15,9 @@
     <body>
       <div class="background">
         <h1>Advices for foreigners in Korea </h1>
-        <form>
-        <label>Username: <input></label></br>
-        <label>Password: <input></label></br>
+        <form action="request/auth/login.php" method="get">
+        <label for="username">Username: <input type="text" name="username" id="username"></label></br>
+        <label for="password">Password: <input type="password" name="password" id="password"></label></br>
         <a href=register.php>Click here for sign up</a></br>
         <input type="submit" value="Log in" >
         </form>
