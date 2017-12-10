1ab75cc651c740aa2ba724f964f0f3d559e34cd2
TRAN <syl_tran@hotmail.fr>
2017-12-10 18:49:04 +0900

    db_config

---
+++ request/db_config.php
@@ -0,0 +1,10 @@
+<?php
+
+/*
+ * All database connection variables
+ */
+
+define('DB_USER', "root"); // db user
+define('DB_PASSWORD', "root"); // db password (mention your db password here)
+define('DB_DATABASE', "final_project"); // database name
+define('DB_SERVER', "localhost"); // db server
